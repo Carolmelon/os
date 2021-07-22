a9c65a9c45aa6cfd913350448f506242efc8d4a0
lrs21 <916583011@qq.com>
2021-07-22 19:57:44 +0800

    移除标准库依赖

---
+++ dump_os.asm
@@ -0,0 +1,20 @@
+
+target/riscv64gc-unknown-none-elf/debug/os:	file format elf64-littleriscv
+
+Sections:
+Idx Name              Size     VMA              Type
+  0                   00000000 0000000000000000 
+  1 .debug_abbrev     000000fd 0000000000000000 
+  2 .debug_info       0000052d 0000000000000000 
+  3 .debug_aranges    00000030 0000000000000000 
+  4 .debug_str        00000443 0000000000000000 
+  5 .debug_pubnames   000000a8 0000000000000000 
+  6 .debug_pubtypes   00000308 0000000000000000 
+  7 .riscv.attributes 00000035 0000000000000000 
+  8 .debug_frame      00000038 0000000000000000 
+  9 .debug_line       00000075 0000000000000000 
+ 10 .comment          00000013 0000000000000000 
+ 11 .symtab           00000078 0000000000000000 
+ 12 .shstrtab         000000a3 0000000000000000 
+ 13 .strtab           00000026 0000000000000000 
+
